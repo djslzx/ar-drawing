022ed1673cdcd6b92fa3ab076b1889148d403001
David J. Lee <deejayessel@gmail.com>
2018-11-23 15:49:24 -0500

    Finished basic Canvas (model) tests

--- Canvas/CanvasTests/CanvasTests.swift
+++ Canvas/CanvasTests/CanvasTests.swift
@@ -12,32 +12,69 @@ import SceneKit
 
 class CanvasTests: XCTestCase {
   
-  func testInit() {
+  private func makeCurves(_ numcurves: Int, _ numpoints: Int) -> [[Point]] {
     var curves : [[Point]] = []
-    for _ in 1...3 {
-      let curve = Array(1...3).map { Point($0, $0, $0) }
+    for _ in 1...numcurves {
+      let curve = Array(1...numpoints).map { Point($0, $0, $0) }
       curves.append(curve)
     }
+    return curves
+  }
+  
+  func testInit() {
+    let c1 = Canvas()
+    let c2 = Canvas()
+    XCTAssertEqual(c1, c2)
+  }
+  
+  func testAddCurve() {
+    let curves = makeCurves(3, 5)
     let c1 = Canvas(curves: curves)
-    
     let c2 = Canvas()
     for curve in curves {
       c2.add(curve: curve)
     }
-    
-    let c3 = Canvas()
-    for curve in curves {
-      c3.startCurve()
-      for point in curve {
-        c3.append(point: point)
-      }
-    }
-    
-    XCTAssertEqual(c1, c2)
-    XCTAssertEqual(c2, c3)
-    XCTAssertEqual(c3, c1)
+    XCTAssertEqual(c1,c2)
   }
   
+  func testAddPoint() {
+    let curves = makeCurves(3, 5)
+    let c1 = Canvas(curves: curves)
+    let c2 = Canvas()
+    for curve in curves {
+      c2.startCurve()
+      for point in curve {
+        c2.append(point: point)
+      }
+    }
+    XCTAssertEqual(c1, c2)
+  }
   
+  func testRemove() {
+    let curves = makeCurves(3, 5)
+    let c1 = Canvas(curves: curves)
+    for _ in 1...curves.count {
+      c1.remove()
+    }
+    let c2 = Canvas()
+    XCTAssertEqual(c1, c2)
+  }
   
+  func testSubscriptPoint() {
+    let curves = makeCurves(3, 5)
+    let c = Canvas(curves: curves)
+    for i in 0..<curves.count {
+      for j in 0..<curves[0].count {
+        XCTAssertEqual(curves[i][j], c[i,j])
+      }
+    }
+  }
+  
+  func testSubscriptCurve() {
+    let curves = makeCurves(3, 5)
+    let c = Canvas(curves: curves)
+    for i in 0..<curves.count {
+      XCTAssertEqual(curves[i], c[i])
+    }
+  }
 }
