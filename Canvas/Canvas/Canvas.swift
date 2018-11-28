4ec0295d91bdfa7da002d7d5989fd6c47506bf60
21djl5 <21djl5@tcl217a-1.cs.williams.edu>
2018-11-27 22:11:21 -0500

    adding PolylineGeometry

--- Canvas/Canvas/Canvas.swift
+++ Canvas/Canvas/Canvas.swift
@@ -37,7 +37,7 @@ public class Polyline : CustomDebugStringConvertible {
   public func removeLast() {
     vertices.removeLast()
   }
- 
+  
   public func lastSegment() -> (float3, float3)? {
     if vertices.count >= 2 {
       return (vertices[vertices.count - 2], vertices[vertices.count - 1])
@@ -56,33 +56,26 @@ public class PolylineGeometry {
   
   private var line : Polyline
   
-  public init() {
-    line = Polyline()
-  }
-  
   public init(vertices : [float3]) {
     line = Polyline(vertices: vertices)
   }
   
-  public var geometry : SCNGeometrySource {
-    var vertices : [SCNVector3] = []
-    for (u,v) in line.segments() {
-      
-    }
-
+  public convenience init() {
+    self.init(vertices: [])
   }
   
-  private func cylinder(from : float3, to : float3, segmentCount : Int) -> [float3] {
-    let polygon : [float3] = []
-    for i in 0..<segmentCount {
-      let point : float3 = 
+  public func geometry(width: CGFloat) -> SCNNode {
+    let parent = SCNNode()
+    for (u, v) in line.segments() {
+      let cylinder = SCNCylinder(radius: width, height: CGFloat(u.distance(to: v)))
+      cylinder.heightSegmentCount = 1
+      cylinder.radialSegmentCount = 6
+      
+      let node = SCNNode(geometry: cylinder)
+      node.simdPosition = u
+      
     }
-
-    for i in 0..<nodes.count {
-      add(node: nodes[i], at: CGPoint(x: cos(2.0 * CGFloat.pi * CGFloat(i) / cgCount),
-                                      y: sin(2.0 * CGFloat.pi * CGFloat(i) / cgCount)))
-    }
-    
+    return parent
   }
 }
 
@@ -95,175 +88,11 @@ public class PolylineGeometry {
 //
 //}
 
-/**
- Represents a collection of curves.
- 
- **Specification Properties**:
- - curves: [[float3]] - A set of curves where each curve is a set of points
- 
- **Derived Specification Properties**:
- 
- **Abstract Invariant**:
- 
- */
-public class Canvas : CustomDebugStringConvertible {
-  
-  // Representation Invariant:
-  //  All curves except the last are non-empty
-  //
-  // Abstraction Function:
-  //  AF(r) = {curve c} such that
-  //   l.start-point = <r.startX, r.startY>
-  //   l.end-point = <r.endX, r.endY>
-  
-  /** The collection of curves stored in the Canvas,
-   where each inner array represents a curve of points
-   connected in order.
-   */
-  private var curves : [[float3]]
-  
-  /**
-   Initializes a Canvas.
-   
-   **Modifies**: self
-   
-   **Effects**: Sets up an empty Canvas.
-   
-   */
-  public init() {
-    curves = []
-    checkRep()
-  }
-  
-  public var debugDescription: String {
-    return String(reflecting: curves)
-  }
-  
-  /**
-   Initializes a Canvas using `curves`.
-   
-   **Modifies**: self
-   
-   **Effects**: Sets up a canvas using data from `curves`.
-   
-   - Parameter curves: A 2-D array of `float3`s
-   where each constituent 1-D array represents a curve consisting of
-   points that are connected in order.
-   
-   */
-  public init(curves: [[float3]]) {
-    let copy : [[float3]] = curves.map {
-      $0.map { float3($0.x, $0.y, $0.z) }
-    }
-    self.curves = copy
-    checkRep()
-  }
-  
-  /**
-   Checks the representation invariant:
-   - All curves except the final one should be non-empty
-   */
-  private func checkRep() {
-    for curve in curves {
-      assert(!curve.isEmpty)
-    }
-  }
-  
-  /**
-   Adds a curve to the Canvas.
-   
-   **Requires**: `curve` is non-empty.
-   
-   **Modifies**: self
-   
-   **Effects**: Adds `curve` to the Canvas.
-   
-   - Parameter curve: The curve to be added to the Canvas.
-   
-   */
-  public func add(curve : [float3]) {
-    assert(!curve.isEmpty)
-    let copy : [float3] = curve.map { return float3($0.x, $0.y, $0.z) }
-    curves.append(copy)
-    checkRep()
-  }
-  
-  /**
-   Adds a point to the most recently added curve in the canvas.
-   If the Canvas is empty, adds a new curve containing the point.
-   
-   **Modifies**: self
-   
-   **Effects**: Adds a point to a curve in the Canvas.
-   
-   - Parameter point: The point to be added.
-   
-   */
-  public func append(point : float3) {
-    if var last = curves.popLast() {
-      last.append(point)
-      curves.append(last)
-      assert(curves.last!.last! == point) // confirm that point has been added
-    } else {
-      curves.append([point])
-    }
-    checkRep()
-  }
-  
-  /**
-   Begins a new empty curve.
-   
-   **Modifies**: self
-   
-   **Effects**: Adds an empty curve to the Canvas.  Has no effect if the
-   most recently added curve is also empty.
-   
-   */
-  public func startCurve() {
-    // Add [] if self.curves is empty or self.curves.last is non-empty
-    if !(curves.last?.isEmpty ?? false) {
-      curves.append([])
-    }
-  }
-  
-  /**
-   Removes the last curve in the Canvas (ordered by time of entry).
-   
-   **Modifies**: self
-   
-   **Effects**: Removes the most recently added curve from the Canvas.
-   
-   */
-  public func remove() {
-    curves.removeLast()
-    checkRep()
-  }
-
-  /**
-   - Returns: A copy of the curves in the Canvas.
-   */
-  public func getCurves() -> [[float3]] {
-    return curves.map {
-      curve in curve.map {
-        float3(x: $0.x, y: $0.y, z: $0.z)
-      }
-    }
-  }
-  
-  /**
-   - Returns: The last line segment added to the Canvas.
-   */
-  public func lastSegment() -> (float3, float3)? {
-    if let last = curves.last, last.count >= 2 {
-      return (last[last.count - 2], last[last.count - 1])
-    } else {
-      return nil
-    }
-  }
-  
-}
-
 extension float3 {
+  
+  public static func - (lhs : float3, rhs : float3) -> float3 {
+    return float3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z)
+  }
   
   public func distance(to dst : float3) -> Float {
     return sqrt(pow(self.x - dst.x, 2) +
